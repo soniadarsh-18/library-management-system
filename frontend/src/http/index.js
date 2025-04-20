3e407dc023858a38c398a9150bad112ee4f7ea78
Adarsh Soni <Adarsh Soni>
2025-04-20 15:55:07 +0530

    Update BASE_URL to point to live backend

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // create instance of axios
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://library-management-system-production-41e8.up.railway.app";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://library-management-system-production-41e8.up.railway.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -236,7 +236,7 @@ api.interceptors.response.use(
       originalRequest.isRetry = true;
       // refresh token request
       try {
-        await axios.get("http://localhost:5000/api/auth/refresh-tokens", {
+        await axios.get("https://library-management-system-production-41e8.up.railway.app/api/auth/refresh-tokens", {
           withCredentials: true,
         });
 
